4b7e27859b7da605de184034df20050c4addd8cf
Jeff Cook <jecook2692@gmail.com>
2020-06-17 12:32:34 -0400

    updates handleClick method

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import './index.css';
 
 
 
-class Square extends React.Component {
 
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
+
+
 
 class Board extends React.Component {
   constructor(props) {
@@ -30,14 +30,14 @@ class Board extends React.Component {
   handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = 'X';
-    this.setState({squares: squares});
+    this.setState({ squares: squares });
   }
   renderSquare(i) {
     return (
-    <Square 
-    value={this.state.squares[i]} 
-    onClick={() => this.handleClick(i)}
-    />
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
     );
   }
 
